f409eb55c82bfc294e472cc54f438bda3889c212
Eduardo Tavares <eduardotavares@Eduardos-MacBook-Air.local>
2023-10-23 17:52:45 -0300

    adjust page spacing

--- src/components/Bootcamps/index.tsx
+++ src/components/Bootcamps/index.tsx
@@ -2,19 +2,18 @@ import { BootcampCard } from './BootcampCard'
 
 export function Bootcamps() {
   return (
-    <section>
+    <section id="bootcamps" className="mb-24 scroll-mt-24">
       <div></div>
 
       <div>
         <ul>
-          <li className="mb-14">
+          <li className="mb-12">
             <BootcampCard
               link="#"
               imageSrc={`/bootcamps/ignite-reactjs.png`}
               altText=""
-              period="jun. 2023 - ago. 2023"
               title="Rocketseat Ignite React.Js"
-              description="Esta é uma trilha de especialização em React.Js. Durante esta jornada desenvolvemos 6 projetos, junto com o instrutor Diego Fernandes, e realizei 6 desafios práticos, apenas aproveitando o layout do Figma desenvolvido pela Rocketseat."
+              description="Esta é a trilha de especialização em React.Js. Durante esta jornada desenvolvemos 6 projetos, junto com o instrutor, Diego Fernandes, e realizei 6 desafios práticos, apenas aproveitando o layout do Figma desenvolvido pela Rocketseat."
               technologies={[
                 'React.js',
                 'Next.Js',
